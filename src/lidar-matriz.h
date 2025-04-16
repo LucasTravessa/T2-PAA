8e32b6ae9ce0fcc8ccdbb523b0cc029eba5423fe
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-04-16 07:55:26 -0300

    Add adjacency matrix loading and printing functionality

---
+++ src/lidar-matriz.h
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int carregar_matriz_ajacencia(int ***mat, int *vertices, int *arestas)
+{
+    int i, j;
+    FILE *arqEntrada = fopen("src/Grafo.txt", "r");
+
+    if (arqEntrada == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return -1;
+    }
+
+    fscanf(arqEntrada, "%d %d", vertices, arestas);
+
+    *mat = malloc(*vertices * sizeof(int *));
+    for (i = 0; i < *vertices; i++)
+        (*mat)[i] = malloc(*vertices * sizeof(int));
+
+    for (i = 0; i < *vertices; i++)
+    {
+        for (j = 0; j < *vertices; j++)
+        {
+            fscanf(arqEntrada, "%d", &((*mat)[i][j]));
+        }
+    }
+
+    fclose(arqEntrada);
+    return 1;
+}
+
+void imprimir_matriz(int **mat, int vertices)
+{
+    for (int i = 0; i < vertices; i++)
+    {
+        for (int j = 0; j < vertices; j++)
+        {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void liberar_memoria_matriz(int **mat, int vertices)
+{
+    for (int i = 0; i < vertices; i++)
+    {
+        free(mat[i]);
+    }
+    free(mat);
+}
--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "./lidar-matriz.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    int **mat_adjacencia;
+    int vertices, arestas;
+
+    if (carregar_matriz_ajacencia(&mat_adjacencia, &vertices, &arestas) != 1)
+    {
+        return 1;
+    }
+
+    printf("\nMatriz de adjacencia:\n");
+    imprimir_matriz(mat_adjacencia, vertices);
+
+    liberar_memoria_matriz(mat_adjacencia, vertices);
     return 0;
 }
