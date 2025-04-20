be301153f889ebdab913cda1851d3f07d78a3d21
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-04-20 18:43:34 -0300

    Add genetic algorithm functions for crossover, mutation, and elitism

---
+++ src/mudanca-genetica.h
@@ -0,0 +1,52 @@
+#include <math.h>
+
+void cruzamento(int *solucao1, int *solucao2, int *novaSolucao, int vertices)
+{
+    int i, pontoCorte1, pontoCorte2;
+    pontoCorte1 = 1 + (rand() % vertices);
+    pontoCorte2 = 1 + (rand() % vertices);
+
+    if (pontoCorte1 > pontoCorte2)
+    {
+        int aux = pontoCorte1;
+        pontoCorte1 = pontoCorte2;
+        pontoCorte2 = aux;
+    }
+
+    for (i = 1; i < vertices; i++)
+    {
+        if (i < pontoCorte1 || i > pontoCorte2)
+        {
+            novaSolucao[i] = solucao1[i];
+        }
+        else
+        {
+            novaSolucao[i] = solucao2[i];
+        }
+    }
+}
+
+void mutacao(int *solucao, int vertices)
+{
+    int i, pontoMutacao;
+    pontoMutacao = 1 + (rand() % vertices);
+
+    // Inverte o valor do ponto de mutação
+    if (solucao[pontoMutacao] == 0)
+        solucao[pontoMutacao] = 1;
+    else
+        solucao[pontoMutacao] = 0;
+}
+
+void elitismo(int **populacao, int tamanhoPopulacao, int *melhorSolucao)
+{
+    int i, indiceMelhor = 0;
+    for (i = 1; i < tamanhoPopulacao; i++)
+    {
+        if (populacao[i][0] < populacao[indiceMelhor][0])
+        {
+            indiceMelhor = i;
+        }
+    }
+    melhorSolucao = populacao[indiceMelhor];
+}
